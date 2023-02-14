f610c13dc3824d8ef580339124badd8b313a23d8
ngetichishmael <ngetichishmael786@gmail.com>
2023-02-14 20:47:43 +0300

    updates to migration

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -24,8 +24,8 @@ class CreateUsersTable extends Migration
             $table->timestamp('email_verified_at')->nullable();
             $table->tinyInteger('status')->default(1);
             $table->string('password');
-            $table->foreignIdFor(Organization::class);
-            $table->foreignIdFor(Role::class);
+            $table->foreignIdFor(Organization::class)->nullable();
+            $table->foreignIdFor(Role::class)->nullable();
             $table->rememberToken();
             $table->timestamps();
         });
