d9a7b660eb26d9de44a02484a58200b5a1faaf41
ngetichishmael <ngetichishmael786@gmail.com>
2023-02-21 07:23:52 +0300

    updates to api

--- routes/api.php
+++ routes/api.php
@@ -24,43 +24,35 @@ Route::group(['namespace' => 'Api'], function () {
 
     Route::post('verify/otp/{number}/{otp}', [AuthenticationController::class, 'verifyOTP']);
     Route::middleware(['auth:sanctum'])->group(function () {
-    Route::prefix('visitors')->group(function () {
-        Route::get('my_all', [VisitorController::class,'index']);
-        Route::get('visitor/{id}', [DriveInController::class,'show']);
-        Route::get('organization-options', [VisitorController::class, 'organizationOptions']);
-        Route::get('identification-options', [VisitorController::class, 'identificationOptions']);
-        Route::get('premises-options', [VisitorController::class,'premisesOptions']);
-        Route::get('purpose-options', [VisitorController::class,'purposeOptions']);
-        Route::get('host-options', [VisitorController::class,'hostOptions']);
-        Route::get('tag-options', [VisitorController::class,'tagOptions']);
-
-        Route::get('drivein/all', [DriveInController::class,'index']);
-        Route::post('drivein/create', [DriveInController::class,'store']);
-
-        Route::get('smsCheckin/all', [SmsCheckInController::class,'index']);
-        Route::post('smsCheckin/create', [SmsCheckInController::class,'store']);
-
-        Route::get('walkin/all', [WalkInController::class,'index']);
-        Route::post('walkin/create', [WalkInController::class,'store']);
-
-        Route::post('verify_checkout', [VisitorController::class,'verifyUser']);
-        Route::put('checkout', [VisitorController::class, 'checkout'])->name('api.visitors.checkout');
-
-
-
-
-        Route::post('phone-number/{phone_number}', [SMSCheckingController::class, 'SMSChecking']);
-        Route::post('verify/otp/{otp}', [SMSCheckingController::class, 'verifyOTP']);
-
-
         Route::prefix('visitors')->group(function () {
-            Route::get('/my_all', [VisitorController::class, 'index']);
+            Route::get('my_all', [VisitorController::class, 'index']);
             Route::get('visitor/{id}', [DriveInController::class, 'show']);
             Route::get('organization-options', [VisitorController::class, 'organizationOptions']);
             Route::get('identification-options', [VisitorController::class, 'identificationOptions']);
             Route::get('premises-options', [VisitorController::class, 'premisesOptions']);
             Route::get('purpose-options', [VisitorController::class, 'purposeOptions']);
+            Route::get('host-options', [VisitorController::class, 'hostOptions']);
             Route::get('tag-options', [VisitorController::class, 'tagOptions']);
+
+            Route::get('drivein/all', [DriveInController::class, 'index']);
+            Route::post('drivein/create', [DriveInController::class, 'store']);
+
+            Route::get('smsCheckin/all', [SmsCheckInController::class, 'index']);
+            Route::post('smsCheckin/create', [SmsCheckInController::class, 'store']);
+
+            Route::get('walkin/all', [WalkInController::class, 'index']);
+            Route::post('walkin/create', [WalkInController::class, 'store']);
+
+            Route::post('verify_checkout', [VisitorController::class, 'verifyUser']);
+            Route::put('checkout', [VisitorController::class, 'checkout'])->name('api.visitors.checkout');
+
+
+
+
+            Route::post('phone-number/{phone_number}', [SMSCheckingController::class, 'SMSChecking']);
+            Route::post('verify/otp/{otp}', [SMSCheckingController::class, 'verifyOTP']);
+
+
             Route::prefix('visitors')->group(function () {
                 Route::get('/my_all', [VisitorController::class, 'index']);
                 Route::get('visitor/{id}', [DriveInController::class, 'show']);
@@ -68,12 +60,21 @@ Route::group(['namespace' => 'Api'], function () {
                 Route::get('identification-options', [VisitorController::class, 'identificationOptions']);
                 Route::get('premises-options', [VisitorController::class, 'premisesOptions']);
                 Route::get('purpose-options', [VisitorController::class, 'purposeOptions']);
-                Route::get('host-options', [VisitorController::class, 'hostOptions']);
                 Route::get('tag-options', [VisitorController::class, 'tagOptions']);
-                Route::get('/drivein/all', [DriveInController::class, 'index']);
-                Route::post('/drivein/create', [DriveInController::class, 'store']);
-                Route::get('/walkin/all', [WalkInController::class, 'index']);
-                Route::post('/walkin/create', [WalkInController::class, 'store']);
+                Route::prefix('visitors')->group(function () {
+                    Route::get('/my_all', [VisitorController::class, 'index']);
+                    Route::get('visitor/{id}', [DriveInController::class, 'show']);
+                    Route::get('organization-options', [VisitorController::class, 'organizationOptions']);
+                    Route::get('identification-options', [VisitorController::class, 'identificationOptions']);
+                    Route::get('premises-options', [VisitorController::class, 'premisesOptions']);
+                    Route::get('purpose-options', [VisitorController::class, 'purposeOptions']);
+                    Route::get('host-options', [VisitorController::class, 'hostOptions']);
+                    Route::get('tag-options', [VisitorController::class, 'tagOptions']);
+                    Route::get('/drivein/all', [DriveInController::class, 'index']);
+                    Route::post('/drivein/create', [DriveInController::class, 'store']);
+                    Route::get('/walkin/all', [WalkInController::class, 'index']);
+                    Route::post('/walkin/create', [WalkInController::class, 'store']);
+                });
             });
         });
     });
