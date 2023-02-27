d2c5a9c8bccf7959838341695d42189b1fa34982
ngetichishmael <ngetichishmael786@gmail.com>
2023-02-27 16:02:27 +0300

    updates to adding users

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -22,7 +22,7 @@ class UserSeeder extends Seeder
             "role_id" => 1,
             'email_verified_at' => now(),
             'password' => '$2y$10$y5KwRinBAUzQsBuK6EvRXuu0i2RrjTHGs00SOD44S1L9VCMgvx3I6',
-            'organization_code'=>'wM1z4nZsIYughLausC6F'
+            'organization_code' => 'wM1z4nZsIYughLausC6F'
 
         ]);
         User::create([
@@ -32,7 +32,7 @@ class UserSeeder extends Seeder
             'email_verified_at' => now(),
             "role_id" => 2,
             'password' => '$2y$10$y5KwRinBAUzQsBuK6EvRXuu0i2RrjTHGs00SOD44S1L9VCMgvx3I6',
-            'organization_code'=>'wM1z4nZsIYughLausC6E'
+            'organization_code' => 'wM1z4nZsIYughLausC6E'
         ]);
         User::create([
             'name' => "Stephen Maina",
@@ -41,24 +41,26 @@ class UserSeeder extends Seeder
             'email_verified_at' => now(),
             "role_id" => 2,
             'password' => '$2y$10$y5KwRinBAUzQsBuK6EvRXuu0i2RrjTHGs00SOD44S1L9VCMgvx3I6',
-            'organization_code'=>'wM1z4nZsIYughLausC6D'
+            'organization_code' => 'wM1z4nZsIYughLausC6D'
         ]);
         Sentry::create([
-            'name'=>'Ishmael ',
-            'status'=>1,
-            'device_id'=>1,
-            'user_detail_id'=>1,
-            'shift_id'=>1,
-            'premise_id'=>1,
+            'name' => 'Ishmael ',
+            'status' => 1,
+            'phone_number' => "0710767015",
+            'device_id' => 1,
+            'user_detail_id' => 1,
+            'shift_id' => 1,
+            'premise_id' => 1,
 
         ]);
         Sentry::create([
-            'name'=>'John Mbugua',
-            'status'=>1,
-            'device_id'=>1,
-            'user_detail_id'=>2,
-            'shift_id'=>1,
-            'premise_id'=>1,
+            'name' => 'John Mbugua',
+            'status' => 1,
+            'device_id' => 1,
+            'phone_number' => "0710767015",
+            'user_detail_id' => 2,
+            'shift_id' => 1,
+            'premise_id' => 1,
 
         ]);
     }
