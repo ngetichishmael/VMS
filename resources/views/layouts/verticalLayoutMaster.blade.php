15741f2fb6dbffcd17b9fba5e7d156b359461879
ngetichishmael <ngetichishmael786@gmail.com>
2023-03-01 22:24:28 +0300

    updates to dashboard

--- resources/views/layouts/verticalLayoutMaster.blade.php
+++ resources/views/layouts/verticalLayoutMaster.blade.php
@@ -55,11 +55,6 @@
     </div>
     <!-- End: Content-->
 
-    @if ($configData['blankPage'] == false && isset($configData['blankPage']))
-        @include('content.pages.customizer')
-
-        {{-- @include('content.pages.buy-now') --}}
-    @endif
 
     <div class="sidenav-overlay"></div>
     <div class="drag-target"></div>
