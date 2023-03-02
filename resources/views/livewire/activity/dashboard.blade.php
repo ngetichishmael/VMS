cb41341a3b90c9f9d2436f8d5f5bf707177a37b5
ngetichishmael <ngetichishmael786@gmail.com>
2023-03-02 11:12:09 +0300

    updates to code

--- resources/views/livewire/activity/dashboard.blade.php
+++ resources/views/livewire/activity/dashboard.blade.php
@@ -9,7 +9,7 @@
                         <div class="input-group-prepend">
                             <span class="input-group-text"><i data-feather="search"></i></span>
                         </div>
-                        <input type="text" id="fname-icon" class="form-control" name="fname-icon"
+                        <input wire:model="search" type="text" id="fname-icon" class="form-control" name="fname-icon"
                             placeholder="Search" />
                     </div>
                 </div>
