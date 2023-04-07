94de796bc8d760492560ae85f115845bae59480a
stephen <stevenmaina17@gmail.com>
2023-04-07 12:46:25 +0300

    removed session

--- app/Http/Middleware/CheckSessionExpiration.php
+++ app/Http/Middleware/CheckSessionExpiration.php
@@ -17,22 +17,22 @@ class CheckSessionExpiration
      */
     public function handle($request, Closure $next)
     {
-        if (!$request->session()) {
-            throw new RuntimeException('Session store not set on request.');
-        }
-        if ($request->session()->has('last_activity')) {
-            $last_activity = $request->session()->get('last_activity');
-            $timeout = config('session.lifetime') * 60;
-
-            if (time() - $last_activity > $timeout) {
-                $request->session()->forget('last_activity');
-                return redirect()->route('/')->with('message', 'Your session has expired. Please log in again.');
-            }
-        }
-
-        $request->session()->put('last_activity', time());
-
-        return $next($request);
+//        if (!$request->session()) {
+//            throw new RuntimeException('Session store not set on request.');
+//        }
+//        if ($request->session()->has('last_activity')) {
+//            $last_activity = $request->session()->get('last_activity');
+//            $timeout = config('session.lifetime') * 60;
+//
+//            if (time() - $last_activity > $timeout) {
+//                $request->session()->forget('last_activity');
+//                return redirect()->route('/')->with('message', 'Your session has expired. Please log in again.');
+//            }
+//        }
+//
+//        $request->session()->put('last_activity', time());
+//
+//        return $next($request);
     }
 
 }
