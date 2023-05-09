9a9f34792a306e2161f554a9b1552a042b9eb087
stephen <stevenmaina17@gmail.com>
2023-05-09 17:15:09 +0300

    blacklist dashboard updates

--- app/Http/Controllers/VisitorController.php
+++ app/Http/Controllers/VisitorController.php
@@ -67,7 +67,7 @@ class VisitorController extends Controller
     public function update(Request $request)
     {
         info(request());
-        dd($request->all());
+        dd($request->query('visitor'));
         $visitor = Visitor::find($request->query('visitor'));
         info("visitor is ".$visitor);
 
