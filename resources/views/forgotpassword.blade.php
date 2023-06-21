75d81c708c61f677f915f38a0f8012e0c68ec348
rickxy <omundieric6@gmail.com>
2023-06-21 14:51:57 +0300

    update

--- resources/views/forgotpassword.blade.php
+++ resources/views/forgotpassword.blade.php
@@ -27,11 +27,11 @@
             <!-- Left Text-->
             <div class="d-none d-lg-flex col-lg-8 align-items-center p-5">
                 <div class="w-100 d-lg-flex align-items-center justify-content-center px-5">
-                    @if ($configData['theme'] === 'dark')
-                        <img class="img-fluid" src="{{ asset('images/pages/login-v2-dark.svg') }}" alt="Login V2" />
-                    @else
-                        <img class="img-fluid" src="{{ asset('images/pages/login-v2.svg') }}" alt="Login V2" />
-                    @endif
+                @if($configData['theme'] === 'dark')
+                <img class="img-fluid" src="{{asset('images/pages/forgot-password-v2-dark.svg')}}" alt="Forgot password V2" />
+                @else
+                <img class="img-fluid" src="{{asset('images/pages/forgot-password-v2.svg')}}" alt="Forgot password V2" />
+                @endif
                 </div>
             </div>
             <!-- /Left Text-->
